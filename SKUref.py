c1d785b6c54042b73ebbfcb1258ea4702e62f127
Kkoder27 <brunswickkm7@gmail.com>
2024-05-02 20:15:50 -0400

    moved reference dictionary and created cost dictionary

---
+++ SKUref.py
@@ -0,0 +1,344 @@
+SKULocations = {
+    'OzAuEagRan' : {
+          'StJP' : {
+              'URL' : 'https://www.stjosephpartners.com/item/1-oz-american-gold-eagle/1EAGLE',
+              'Quantity': {
+                  '1-9' : '/html/body/div[1]/main/div/div[1]/div[2]/div[2]/div[2]/div/div/table/tbody/tr[2]/td[2]',
+                  '10-19' : '/html/body/div[1]/main/div/div[1]/div[2]/div[2]/div[2]/div/div/table/tbody/tr[3]/td[2]',
+                  '20-49' : '/html/body/div[1]/main/div/div[1]/div[2]/div[2]/div[2]/div/div/table/tbody/tr[4]/td[2]',
+                  '50+' : '/html/body/div[1]/main/div/div[1]/div[2]/div[2]/div[2]/div/div/table/tbody/tr[5]/td[2]'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzAuEagCur' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzAuMapleCur' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzAuPhilCur' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzAuBarCoice' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzArEagCur' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzArMapleCur' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'OzArBuffalo' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        },
+    'TenOzArBar' : {
+          'StJP' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'APMEX' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'JMBullion': {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+          },
+          'SDBullion' : {
+              'URL' : 'get',
+              'Quantity': {
+                  '1-9' : 'pass',
+                  '10-19' : 'pass',
+                  '20-49' : 'pass',
+                  '50+' : 'pass'
+                  }
+            }
+        }
+}
